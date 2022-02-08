52e7b46b79720c0ad28dce78f47929b12448ab6b
NorthernSantan <colin.william.mccormack@gmail.com>
2022-02-07 21:17:38 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by colin on 2/7/2022.
+//
+
+/*
+ *
+ * Preparation for waterloo ccc
+ *
+ * Practice using cpp for competition
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int arr[] = {9,3,4,6,7,9};
+
+    int n = 6;
+
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
