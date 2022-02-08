9df6d6aef1ee98d40239e67b16cb0d494da37902
NorthernSantan <colin.william.mccormack@gmail.com>
2022-02-07 21:37:32 -0500

    initial commit

--- main.cpp
+++ main.cpp
@@ -14,14 +14,30 @@
 
 using namespace std;
 
+// Match type for vector since it is not an array
+void print (vector<int> a, int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+}
+
 int main() {
-    int arr[] = {9,3,4,6,7,9};
+
+    // Optimization to remove c style input output
+    ios_base::sync_with_stdio(false);
+    // Optimization to prevent buffer flushing automatically
+    cin.tie(nullptr);
+
+    vector<int> arr(6);
+
+    arr = {9,3,4,6,7,9};
+
+    cout << arr.size() << "\n";
 
     int n = 6;
 
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i] << " ";
-    }
+    print (arr, n);
 
     return 0;
+
 }
